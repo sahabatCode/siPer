a1f4cbd8888a2dcb6c7aac90a4d861a106a11dda
kageNinja <haviedzmiftah@gmail.com>
2015-09-27 15:57:53 +0700

    member

---
+++ mod/data_master/member/member_tampil.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+if (empty($_SESSION['username']) AND empty($_SESSION['password'])){
+?>
+   <script>alert('Untuk mengakses halaman admin, Anda harus login terlebih dahulu.'); window.location = './index.php'</script>
+<?php
+}
+else{
+  if ($_SESSION[status]=='admin'){
+    $aksi="mod/data_master/member/kategori_aksi.php";
+  ?>
+    <div class="judul"><h2>Data Member</h2></div>
+    <div class="tambah"><input type="button" class="button" value="Tambah Data" onclick="mem_tb()"></div>
+    <div class="area_main"><!-- class area_main -->											
+      <table class="judul" >
+        <tr>
+          <th width="20px">no.</th>
+          <th width="200px">nama member</th>
+          <th width="200">Alamat</th>
+          <th width="200">No Hp</th>
+          <th width="80px" align="center">aksi</th>
+        </tr>
+      </table>
+      <div class="data"><!-- class data -->
+      <table class="data">
+      <?php
+      $no=1;
+
+      $mem = mysql_query("SELECT * FROM member");
+      $cek_mem = mysql_num_rows($mem);
+
+      if ($cek_mem > 0){
+        while($y = mysql_fetch_array($mem)){
+	  ?>
+          <tr>
+            <td width="20px" align="center"><?php echo $no; ?></td>
+            <td width="200px"><?php echo $y['nama']; ?></td>
+            <td width="200"><?php echo $y['alamat']; ?></td>
+            <td width="200"><?php echo $y['hp']; ?></td>
+            <td width="80px" align="center">
+			  <a href="?mod=kat_ub&id=<?php echo $y['id_kat']; ?>"><img src="images/edit.png" title="edit"></a>
+              <a href="<?php echo "$aksi?mod=kat&aksi=hap_dt&id=$y[id_kat]"; ?>" onclick="return confirm('Anda yakin ingin menghapus data <?php echo $y['nama_kat']; ?>')"><img src="images/delete.png" title="Hapus"></a>
+            </td>
+          </tr>
+      <?php
+        $no++;
+        }
+      }
+      else{
+      ?>
+        <tr><td colspan="3"><b><center>DATA KATEGORI BELUM TERSEDIA</center></b></td></tr>
+      <?php
+      }
+      ?>
+      </table>
+      </div><!-- end class data -->
+      <div class="jml_data">Jumlah Data : <?php echo $cek_kat; ?></div>
+    </div><!-- end class area_main -->
+    <?php
+  }
+}
+?>
