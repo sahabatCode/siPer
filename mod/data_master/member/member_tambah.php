e730e5afa2ff4cc7540048b6b3d54756c0cb27d8
kageNinja <haviedzmiftah@gmail.com>
2015-09-27 15:48:51 +0700

    edit-edit

--- mod/data_master/member/member_tambah.php
+++ mod/data_master/member/member_tambah.php
@@ -7,94 +7,57 @@ if (empty($_SESSION['username']) AND empty($_SESSION['password'])){
 }
 else{
   if ($_SESSION['status']=='admin'){
-    $aksi="mod/data_master/barang/barang_aksi.php";
+    $aksi="mod/data_master/member/member_aksi.php";
   ?>
     <div class="judul"><h2>Tambah Member</h2></div>
     <div class="area_main">
-      <form method="POST" action="<?php echo "$aksi?mod=brg&aksi=tb_dt"; ?>">
+      <form method="POST" action="<?php echo "$aksi?mod=mem&aksi=tb_dt"; ?>">
         <table class="form">
 		  
 		  <tr>
-            <td>Merk</td>
+            <td>Id Member</td>
             <td>:</td>
-            <td>
-              <select name="merk" style="width:130px;">
-				<option selected="selected" value="">- Pilih Merk -</option>
-				<?php
-				$merk=mysql_query("SELECT * FROM merk ORDER BY id_merk DESC");		  
-				if (mysql_num_rows($merk)>0) {
-				  while ($m=mysql_fetch_array($merk)){
-				?>
-				    <option value="<?php echo $m['id_merk']; ?>"><?php echo $m['nama_merk']; ?></option>
-                <?php
-				}
-			  }
-              ?>
-		      </select>         
-            </td>
+            <td><input type="text" name="id_member" value="" size="10" /></td>
           </tr>
 		  <tr>
-            <td>Kategori</td>
+            <td>Nama</td>
             <td>:</td>
-            <td>
-              <select name="kategori" style="width:130px;">
-				<option selected="selected" value="">- Pilih Kategori -</option>
-				<?php
-				$kat=mysql_query("SELECT * FROM kategori ORDER BY id_kat DESC");		  
-				if (mysql_num_rows($kat)>0) {
-				  while ($k=mysql_fetch_array($kat)){
-				?>
-				    <option value="<?php echo $k['id_kat']; ?>"><?php echo $k['nama_kat']; ?></option>
-                <?php
-				}
-			  }
-              ?>
-		      </select>         
-            </td>
+            <td><input type="text" name="nama" id="nama" value="" size="10" /></td>
           </tr>
 		  <tr>
-            <td>Satuan</td>
+            <td>Tempat Lahir</td>
             <td>:</td>
-            <td>
-              <select name="satuan" style="width:130px;">
-				<option selected="selected" value="">- Pilih Satuan -</option>
-				<?php
-				$sat=mysql_query("SELECT * FROM satuan ORDER BY id_sat DESC");		  
-				if (mysql_num_rows($sat)>0) {
-				  while ($s=mysql_fetch_array($sat)){
-				?>
-				    <option value="<?php echo $s['id_sat']; ?>"><?php echo $s['nama_sat']; ?></option>
-                <?php
-				}
-			  }
-              ?>
-		      </select>         
-            </td>
+            <td><input type="text" name="tmp_lahir" /></td>
           </tr>
 		  <tr>
-            <td>Harga Beli</td>
+            <td>Tanggal Lahir</td>
             <td>:</td>
-            <td><input type="text" name="harga_beli" id="harga_beli" value="" size="10" /></td>
+            <td><input type="text" name="tgl_lahir" value="" size="10" /></td>
           </tr>
 		  <tr>
-            <td>Harga Jual</td>
+            <td>Alamat </td>
             <td>:</td>
-            <td><input type="text" name="harga_jual" id="harga_jual" value="" size="10" /></td>
+            <td><input type="text" name="alamat" value="" size="10" /></td>
           </tr>
 		  <tr>
-            <td>Barcode</td>
+            <td>Email</td>
             <td>:</td>
-            <td><input type="text" name="barcode" id="barcode" value="" size="10" maxlength="10" /></td>
+            <td><input type="text" name="email" value="" size="10" maxlength="10" /></td>
           </tr>
 		  <tr>
-            <td>Stock</td>
+            <td>Telephon</td>
             <td>:</td>
-            <td><input type="text" name="stock" id="stock" value="" size="5px" /></td>
+            <td><input type="text" name="telphon" value="" size="5px" /></td>
           </tr>
+      <tr>
+        <td>Hp</td>
+        <td>:</td>
+        <td><input type="text" name="hp" value="" size="10" /></td>
+      </tr>
           <tr>
             <td></td>
             <td></td>
-            <td><input type="submit" class="button" value="Simpan"> <input type="button" class="button" value="Batal" onclick="brg_bat()"></td>
+            <td><input type="submit" class="button" value="Simpan"> <input type="button" class="button" value="Batal" onclick="mem_bat()"></td>
           </tr>
         </table>
 	  </form>
